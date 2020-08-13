4418797781dd5d9034a535ca2fef7c0df1e37998
linglcioc7 <59540006+linglcioc7@users.noreply.github.com>
2020-08-13 11:18:44 +0800

    Create 123.js

---
+++ 123.js
@@ -0,0 +1,15 @@
+const cookieName = 'sit'
+const cookieKey = 'ling_cookie_sit'
+const cookieVal = $request.headers['Cookie']
+
+if (cookieVal) {
+  let cookie = $prefs.setValueForKey(cookieVal, cookieKey)
+  if (cookie) {
+    let msg = `${cookieName}`
+    $notify(msg, 'Cookie写入成功', '详见日志')
+    console.log(msg)
+    console.log(cookieVal)
+  }
+}
+
+$done({})
