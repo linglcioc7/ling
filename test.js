6bd287c0e4af1705dfa3d0b0a33a685dac32cffc
linglcioc7 <59540006+linglcioc7@users.noreply.github.com>
2020-08-13 10:02:50 +0800

    Update test.js

--- test.js
+++ test.js
@@ -33,21 +33,3 @@ $task.fetch(myRequest).then(response => {
     // reason.error
     $notify("Title", "Subtitle", reason.error); // Error!
 });
-
-{
-    "url": "http://210.35.96.114/report/report/todayReport",
-    "raw_url": "http://210.35.96.114/report/report/todayReport",
-    "method": "post",
-    "headers": {
-        "Origin": "http://xgfy.sit.edu.cn",
-        "Content-Type": "application/json",
-        "ts": "1595817949995",
-        "Host": "210.35.96.114",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15",
-        "Referer": "http://xgfy.sit.edu.cn/",
-        "decodes": "3D362A3E5456C66FBCB5D57977C86E31"
-    },
-    "data": {
-        "{\"wendu\":0,\"jttw\":-1,\"ksfl\":0,\"usercode\":\"171012Y118\",\"username\":\"周兵\",\"usertype\":2,\"jiguan\":3,\"currentsituation\":3,\"remarks\":\"\",\"deptno\":\"200100\",\"position\":\"浙江省-杭州市\",\"fanxiang\":0,\"lvyou\":0,\"jiechu\":0,\"jiechuqy\":0,\"isback\":0,\"jiaotongfangshi\":0,\"isinhubei\":0,\"notforegoing\":1,\"id\":3296191}": ""
-    }
-}
